fa038e4821593b60459f0b1fd4c9adbb5ca1941d
Matt York <my0373@gmail.com>
2015-12-22 19:10:50 +0000

    Delete promote-content-views.sh
    Removed nonsense script.

--- promote-content-views.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# -lt 3 ]
-then
-	echo "promote-content-views.sh [ORG_ID] [CONTENT_VIEW_ID] [LIFECYCLE_ENVIRONMENT_STAGE_ID]"
-	exit 1
-fi
-
-## List all of the hammer products
-hammer content-view list --organization-id $1
